5d9ef7397bdf28cd36e490887a89a91056606fef
tapader13 <minhajtapader0@gmail.com>
2025-02-20 00:58:59 -0500

    sortable work

--- src/components/home/TaskCard.jsx
+++ src/components/home/TaskCard.jsx
@@ -1,6 +1,22 @@
+import { useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
+
 export default function TaskCard({ task }) {
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id: task._id });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
   return (
-    <div className='bg-white rounded-lg shadow-md p-4 hover:shadow-lg transition-shadow duration-200'>
+    <div
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      {...listeners}
+      className='bg-white rounded-lg shadow-md p-4 hover:shadow-lg transition-shadow duration-200'
+    >
       <h3 className='font-semibold mb-2 text-purple-700'>{task.title}</h3>
       <p className='text-sm text-gray-600 mb-2'>{task.description}</p>
       <div className='flex justify-between items-center text-xs text-gray-500'>
--- src/components/home/TaskColumn.jsx
+++ src/components/home/TaskColumn.jsx
@@ -1,14 +1,19 @@
 import { useEffect, useState } from 'react';
 import TaskCard from './TaskCard';
 import axios from 'axios';
+import { closestCorners, DndContext } from '@dnd-kit/core';
+import {
+  SortableContext,
+  verticalListSortingStrategy,
+} from '@dnd-kit/sortable';
 
 export default function TaskColumn({ category }) {
-  const [task, tasks] = useState([]);
+  const [task, setTask] = useState([]);
   useEffect(() => {
     const fetchTasks = async () => {
       const res = await axios.get('http://localhost:5001/tasks');
       if (res.data.success) {
-        tasks(res.data.data);
+        setTask(res.data.data);
       }
     };
     fetchTasks();
@@ -17,13 +22,22 @@ export default function TaskColumn({ category }) {
   const filteredTasks =
     task && task.filter((task) => task.category === category);
   return (
-    <div className='bg-white bg-opacity-20 backdrop-filter backdrop-blur-lg rounded-lg p-4 shadow-lg'>
-      <h2 className='text-xl font-semibold mb-4 text-purple-700'>{category}</h2>
-      <div className='space-y-4'>
-        {filteredTasks?.map((task) => (
-          <TaskCard key={task.id} task={task} />
-        ))}
-      </div>
-    </div>
+    <DndContext collisionDetection={closestCorners}>
+      <SortableContext
+        strategy={verticalListSortingStrategy}
+        items={filteredTasks.map((task) => task._id)}
+      >
+        <div className='bg-white bg-opacity-20 backdrop-filter backdrop-blur-lg rounded-lg p-4 shadow-lg'>
+          <h2 className='text-xl font-semibold mb-4 text-purple-700'>
+            {category}
+          </h2>
+          <div className='space-y-4'>
+            {filteredTasks?.map((task) => (
+              <TaskCard key={task._id} task={task} />
+            ))}
+          </div>
+        </div>
+      </SortableContext>
+    </DndContext>
   );
 }
